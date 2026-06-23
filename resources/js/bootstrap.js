10e74666aa1396d2d663477fb4005f54c60c5ba1
heris.buds@gmail.com <heriabunizar@gmail.com>
2026-06-23 09:55:48 +0700

    next

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,7 @@ window._ = require('lodash');
 
 try {
     require('bootstrap');
-} catch (e) {}
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -30,3 +30,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+// Sanctum CSRF + Axios Config
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios = axios;
